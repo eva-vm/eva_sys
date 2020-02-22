d71c0122405ca9e762fabaab481eb1761cb1f423
Arthur Correnson <arthur.correnson@gmail.com>
2020-02-22 17:43:01 +0100

    make folders

---
+++ std/std.s
@@ -0,0 +1,11 @@
+@ ================================
+@ Minimal Standard Lib for Eva
+@ Author : Arthur Correnson
+@ ================================
+
+@ Ascii to Integer conversion
+eva_atoi:
+
+
+@ Integer to ascii conversion
+eva_itoa:
